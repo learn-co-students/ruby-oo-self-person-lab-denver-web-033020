0863eab59d3e4621c49faba677c68a5d92f4cdc7
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2020-04-04 20:39:40 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
-# your code goes here
+class Person
+
+    attr_reader :name
+    @@bank_account = 25
+    @@happiness_poimts = 8
+    @@hygiene_points = 8
+
+    def initialize name
+        @name = name
+    end
+
+end
